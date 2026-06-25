0c51953fc0d6e8d27effac0c2ae9acf7bbb2d2c5
cake <ms8042939@gmail.com>
2026-06-25 12:07:33 +0100

    add rules page

---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<article>
+	<section>
+		<h1>Sobre a OutKing Series</h1>
+		<p>
+			A OutKing Series surgiu em 2024 com o objetivo de desenvolver a área competitiva de Valorant
+			em Portugal. Desde a sua criação, já existiram 3 edições que consistiram em mais de 50 jogos
+			entre muitas equipas do país.
+		</p>
+		<p>
+			Em 2026, a OutKing Series regressa com novas parcerias que visam a melhorar a qualidade da
+			experiência para as equipas participantes, os espectadores e a equipa que desenvolve a
+			competição.
+		</p>
+	</section>
+	<section>
+		<h1>As Equipas</h1>
+		<h2>Composição</h2>
+		<p>
+			As equipas participantes no torneio devem ser compostas por um mínimo de <b>5</b> jogadores.
+			Cada equipa pode ter um total de <b>7</b> jogadores. (5 principais, 2 substitutos).
+		</p>
+		<p>
+			A equipa pode, também, inscrever um coach (treinador). O coach pode ser um jogador desde que a
+			equipa
+			<b>não exceda o máximo de jogadores</b> descrito acima
+		</p>
+		<p>
+			Para além dos jogadores e coach, a equipa pode também inscrever a sua equipa técnica com o
+			máximo de <b>2</b>
+			membros, que <b>não poderão jogar ou estar dentro do jogo</b>, estando esse lugar reservado
+			exclusivamente ao coach.
+		</p>
+		<h2>Localização/Nacionalidade</h2>
+		<p>
+			Em edições prévias da OutKing Series, era um requisito de formação de equipa que fosse
+			composta por pelo menos 3 jogadores residentes/cidadãos em/de Portugal.
+			<b>
+				Este requisito já não se encontra em efeito, pelo que as equipas poderão ser formadas por
+				jogadores de qualquer nacionalidade e/ou residência, sem restrições quaisquer.
+			</b>
+		</p>
+		<h2>Ranking</h2>
+		<p>
+			Os jogadores que pretendem inscrever-se no torneio deverão ter, à data de inscrição, um rank
+			mínimo de <b>Ascendente 3</b> no modo competitivo. A quebra desta regra está sob pena do impedimento
+			de jogar no torneio e da desqualificação da equipa (caso não cumpra com o mínimo de jogadores).
+		</p>
+		<h2>Inscrições</h2>
+		<p>
+			O processo de inscrição deve ser seguido por todas as equipas participantes, inclusive
+			convidadas. Para inscrever a sua equipa, clique
+			<b>
+				<a href={resolve('/register')}>aqui</a>
+			</b>.
+		</p>
+	</section>
+</article>
